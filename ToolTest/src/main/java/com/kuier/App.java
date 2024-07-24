37f3dd601d0376b8cf4f48115369626b39623b38
LiuQi <1139642444@qq.com>
2024-07-24 17:54:53 +0800

    1.新增 ToolAll模块 2.新增 ToolTest模块 3.pom.xml 文件 新增 org.junit.vintage 全局测试依赖

---
+++ ToolTest/src/main/java/com/kuier/App.java
@@ -0,0 +1,16 @@
+package com.kuier;
+
+import junit.framework.TestCase;
+
+/**
+ * @ClassName App
+ * @Description App
+ * @Author LiuQi
+ * @Date 2024/7/24 17:51
+ * @Version 1.0
+ */
+public class App extends TestCase {
+    public void testApp() {
+        System.out.println("Test App!");
+    }
+}
