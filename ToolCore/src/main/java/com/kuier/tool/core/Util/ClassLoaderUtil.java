e4f13e6a87b8893b27350a549a75d8243192b16f
LiuQi <1139642444@qq.com>
2024-08-02 10:30:19 +0800

    ToolCore模块新增 1. ObjectUtil 类新增 以及defaultIfNull 参数为空默认值,isNull 参数是否为空 等方法 2. ClassLoaderUtil 类 新增 以及  getClassLoader 获取类加载器,getContextClassLoader 获取上下文环境类加载器,getSystemClassLoader 获取系统类加载器 等方法

---
+++ ToolCore/src/main/java/com/kuier/tool/core/Util/ClassLoaderUtil.java
@@ -0,0 +1,67 @@
+package com.kuier.tool.core.Util;
+
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
+/**
+ * @ClassName ClassLoaderUtil
+ * @Description ClassLoaderUtil 类加载工具类
+ * @Author LiuQi
+ * @Date 2024/7/29 14:33
+ * @Version 1.0
+ */
+public class ClassLoaderUtil {
+
+    /**
+     * getClassLoader 获取类加载器
+     *
+     * @return
+     * @author LiuQi
+     */
+    public static ClassLoader getClassLoader() {
+        // 获取当前线程的上下文类加载器
+        ClassLoader classLoader = getContextClassLoader();
+        if (classLoader == null) { // 上下文类加载器为空,则获取当前类加载器
+            // 获取当前类的类加载器
+            classLoader = ClassLoaderUtil.class.getClassLoader();
+            if (null == classLoader) { // 当前类的类加载器为空,则获取系统类加载器
+                // 获取系统类加载器
+                classLoader = getSystemClassLoader();
+            }
+        }
+        // 返回类加载器
+        return classLoader;
+    }
+
+    /**
+     * getContextClassLoader 获取上下文环境类加载器
+     *
+     * @return
+     * @author LiuQi
+     */
+    public static ClassLoader getContextClassLoader() {
+        if (System.getSecurityManager() == null) { // 获取当前应用程序的安全管理器为空
+            // 直接返回当前线程的上下文类加载器
+            return Thread.currentThread().getContextClassLoader();
+        } else { // 绕开权限检查
+            // 授权机制 作用是为了防止恶意代码或其他不安全的代码获取系统中的敏感信息或执行不允许的操作
+            return AccessController.doPrivileged((PrivilegedAction<ClassLoader>) () -> Thread.currentThread().getContextClassLoader());
+        }
+    }
+
+    /**
+     * getSystemClassLoader 获取系统类加载器
+     *
+     * @return
+     * @author LiuQi
+     */
+    public static ClassLoader getSystemClassLoader() {
+        if (System.getSecurityManager() == null) { // 获取当前应用程序的安全管理器为空
+            // 直接返回系统类加载器
+            return ClassLoader.getSystemClassLoader();
+        } else { // 绕开权限检查
+            // 授权机制 作用是为了防止恶意代码或其他不安全的代码获取系统中的敏感信息或执行不允许的操作
+            return AccessController.doPrivileged((PrivilegedAction<ClassLoader>) ClassLoader::getSystemClassLoader);
+        }
+    }
+}
---
+++ ToolCore/src/main/java/com/kuier/tool/core/Util/ObjectUtil.java
@@ -0,0 +1,43 @@
+package com.kuier.tool.core.Util;
+
+import java.util.function.Supplier;
+
+/**
+ * @ClassName ObjectUtil
+ * @Description ObjectUtil Object 工具类
+ * @Author LiuQi
+ * @Date 2024/7/29 14:22
+ * @Version 1.0
+ */
+public class ObjectUtil {
+
+    /**
+     * defaultIfNull 参数为空默认值
+     *
+     * @param param
+     * @param defaultValueSupplier
+     * @param <T>
+     * @return
+     * @author LiuQi
+     */
+    public static <T> T defaultIfNull(T param, Supplier<? extends T> defaultValueSupplier) {
+        // 参数是否为空
+        if (isNull(param)) {// 参数为空时返回默认值
+            return defaultValueSupplier.get();
+        }
+        // 参数不为空时返回参数
+        return param;
+    }
+
+    /**
+     * isNull 参数是否为空
+     *
+     * @param param
+     * @return
+     * @author LiuQi
+     */
+    public static Boolean isNull(Object param) {
+        // 为空返回 true 否则返回 false
+        return null == param || param.equals(null);
+    }
+}
