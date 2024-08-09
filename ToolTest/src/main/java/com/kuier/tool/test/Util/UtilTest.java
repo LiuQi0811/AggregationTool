ea437b1d7efd6d83205b8e83cd89e14fad749d1a
LiuQi <1139642444@qq.com>
2024-08-09 08:47:19 +0800

    ToolTest模块新增 1. UtilTest 工具类 单元测试 新增 Obj 工具类 Type 工具类 Coll 工具类  Number 工具类 Array 工具类测试相关方法

--- ToolTest/src/main/java/com/kuier/tool/test/Util/UtilTest.java
+++ ToolTest/src/main/java/com/kuier/tool/test/Util/UtilTest.java
@@ -3,6 +3,10 @@ package com.kuier.tool.test.Util;
 import com.kuier.tool.core.Util.*;
 import junit.framework.TestCase;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @ClassName UtilTest
  * @Description UtilTest 工具类 单元测试
@@ -12,16 +16,78 @@ import junit.framework.TestCase;
  */
 public class UtilTest extends TestCase {
     /**
-     * 测试 Object 工具类
+     * 测试 Obj 工具类
      *
      * @author LiuQi
      */
     public void testObjectUtil() {
-        System.out.println(" 测试 Object 工具类: " + ObjUtil.defaultIfNull(null, Object::new));
-        System.out.println(" 测试 Object 工具类 isNull 参数是否为空:  " + ObjUtil.isNull(null));
-        System.out.println(" 测试 Object 工具类: " + ObjUtil.defaultIfNull(ClassLoader.class, null));
-        System.out.println(" 测试 Object 工具类: " + ObjUtil.defaultIfNull(ClassLoader.class, null));
-        System.out.println(" 测试 Object 工具类: " + ObjUtil.defaultIfNull(ClassLoader.class, null));
+        System.out.println(" 测试 Obj 工具类: " + ObjUtil.defaultIfNull(null, Object::new));
+        System.out.println(" 测试 Obj 工具类 isNull 参数是否为空:  " + ObjUtil.isNull(null));
+        System.out.println(" 测试 Obj 工具类 isNull 参数是否为空:  " + ObjUtil.isNull(""));
+        System.out.println(" 测试 Obj 工具类: " + ObjUtil.defaultIfNull(ClassLoader.class, null));
+        System.out.println(" 测试 Obj 工具类: " + ObjUtil.defaultIfNull(ClassLoader.class, null));
+        System.out.println(" 测试 Obj 工具类: " + ObjUtil.defaultIfNull(ClassLoader.class, null));
+        Map<String, String> stringMap = new HashMap<>();
+        stringMap.put("number", "123");
+        System.out.println(" 测试 Obj 工具类: toString 对象转字符串 " + ObjUtil.toString(stringMap));
+        System.out.println(" 测试 Obj 工具类: toString 对象转字符串 " + ObjUtil.toString("123"));
+        System.out.println(" 测试 Obj 工具类: ");
+        System.out.println(" 测试 Obj 工具类: ");
+        System.out.println(" 测试 Obj 工具类: ");
+        System.out.println(" 测试 Obj 工具类: ");
+        System.out.println(" 测试 Obj 工具类: ");
+        System.out.println(" 测试 Obj 工具类: ");
+        System.out.println(" 测试 Obj 工具类: ");
+        System.out.println(" 测试 Obj 工具类: ");
+        System.out.println(" 测试 Obj 工具类: ");
+    }
+
+    /**
+     * 测试 Type 工具类
+     *
+     * @author LiuQi
+     */
+    public void testTypeUtil() {
+        System.out.println(" 测试 Type 工具类 isUnknown 是否为未知类型: " + TypeUtil.isUnknown(Integer.class));
+        System.out.println(" 测试 Type 工具类 getClass 获取Type对应的映射类: " + TypeUtil.getClass(Integer.class));
+        System.out.println(" 测试 Type 工具类: ");
+        System.out.println(" 测试 Type 工具类: ");
+        System.out.println(" 测试 Type 工具类: ");
+        System.out.println(" 测试 Type 工具类: ");
+        System.out.println(" 测试 Type 工具类: ");
+        System.out.println(" 测试 Type 工具类: ");
+        System.out.println(" 测试 Type 工具类: ");
+    }
+
+    /**
+     * 测试 Array 工具类
+     *
+     * @author LiuQi
+     */
+    public void testArrayUtil() {
+        System.out.println(" 测试 Array 工具类: " + ArrayUtil.isArray(Arrays.asList("1", "2", "3").toArray()));
+        System.out.println(" 测试 Array 工具类: ");
+        System.out.println(" 测试 Array 工具类: ");
+        System.out.println(" 测试 Array 工具类: ");
+        System.out.println(" 测试 Array 工具类: ");
+        System.out.println(" 测试 Array 工具类: ");
+        System.out.println(" 测试 Array 工具类: ");
+        System.out.println(" 测试 Array 工具类: ");
+    }
+
+    /**
+     * 测试 Coll 工具类
+     *
+     * @author LiuQi
+     */
+    public void testCollUtil() {
+        System.out.println(" 测试 Coll 工具类: ");
+        System.out.println(" 测试 Coll 工具类: ");
+        System.out.println(" 测试 Coll 工具类: ");
+        System.out.println(" 测试 Coll 工具类: ");
+        System.out.println(" 测试 Coll 工具类: ");
+        System.out.println(" 测试 Coll 工具类: ");
+        System.out.println(" 测试 Coll 工具类: ");
     }
 
     /**
@@ -56,9 +122,9 @@ public class UtilTest extends TestCase {
         System.out.println("测试 CharSequence 工具类 subSuf 截取指定位置后面的字符串 : " + StrUtil.subSuf("Hello World", 4));
         System.out.println("测试 CharSequence 工具类 trimStart   : " + StrUtil.trimStart(" Hello World ！"));
         System.out.println("测试 CharSequence 工具类 trimStart   : " + " Hello World ！");
-        System.out.println("测试 CharSequence 工具类   : ");
-        System.out.println("测试 CharSequence 工具类   : ");
-        System.out.println("测试 CharSequence 工具类   : ");
+        System.out.println("测试 CharSequence 工具类  trim :" + StrUtil.trim(" Wa HaHa "));
+        System.out.println("测试 CharSequence 工具类 isEmptyIfStr 字符串为空处理  : " + StrUtil.isEmptyIfStr(null));
+        System.out.println("测试 CharSequence 工具类  split 字符串分割（含默认分片）  : " + StrUtil.split("1,2,3,4", ','));
         System.out.println("测试 CharSequence 工具类   : ");
         System.out.println("测试 CharSequence 工具类   : ");
         System.out.println("测试 CharSequence 工具类   : ");
@@ -86,4 +152,22 @@ public class UtilTest extends TestCase {
         System.out.println("测试 file 工具类   : ");
         System.out.println("测试 file 工具类   : ");
     }
+
+    /**
+     * 测试 Number 工具类
+     *
+     * @author LiuQi
+     */
+    public void testNumberUtil() {
+        System.out.println(" 测试 NumberUtil 工具类   : " + NumberUtil.equals('A', 'a', true));
+        System.out.println(" 测试 NumberUtil 工具类   : ");
+        System.out.println(" 测试 NumberUtil 工具类   : ");
+        System.out.println(" 测试 NumberUtil 工具类   : ");
+        System.out.println(" 测试 NumberUtil 工具类   : ");
+        System.out.println(" 测试 NumberUtil 工具类   : ");
+        System.out.println(" 测试 NumberUtil 工具类   : ");
+        System.out.println(" 测试 NumberUtil 工具类   : ");
+    }
+
+
 }
